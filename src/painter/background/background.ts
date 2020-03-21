8ab63cb4d11b15ae3ebf8b98b4d0c6fa7f34ac82
Tolokoban <contact@tolokoban.org>
2020-03-21 22:42:40 +0100

    Background painter is still buggy

--- src/painter/background/background.ts
+++ src/painter/background/background.ts
@@ -61,7 +61,7 @@ export default class BackgroundPainter extends Painter {
         prg.setUniform('uniImageAspectRatio', atlas.width / atlas.height)
         prg.setUniform('uniAlignX', alignX)
         prg.setUniform('uniAlignY', alignY)
-        prg.setUniform('uniScale', scale)
+        prg.setUniform('uniScale', 1 / scale)
         prg.bindAttribs(buff, 'attXY')
         gl.bindBuffer(gl.ARRAY_BUFFER, buff)
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, NB_VERTICES_IN_SQUARE)
@@ -75,8 +75,6 @@ export default class BackgroundPainter extends Painter {
     }
 
     protected initialize(scene: Scene) {
-        console.log(vert)
-        console.log(frag)
         this.prg = this.createProgram({ frag, vert })
         const { gl } = scene
         const buff = gl.createBuffer()
